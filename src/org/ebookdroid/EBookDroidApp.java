69c37348bf7c26f280b5aa162eda4848dd60b9d0
mc.creat <mc.creat@714301b5-5bf2-3aa6-4ccc-ecebab0412e3>
2011-08-21 06:38:06 +0000

    Disable EmergencyHandler. Currently not usefull, couse store file in private space Also FC in some devices. Need additional work.
    git-svn-id: http://ebookdroid.googlecode.com/svn/trunk@173 714301b5-5bf2-3aa6-4ccc-ecebab0412e3

--- src/org/ebookdroid/EBookDroidApp.java
+++ src/org/ebookdroid/EBookDroidApp.java
@@ -1,6 +1,6 @@
 package org.ebookdroid;
 
-import org.ebookdroid.core.log.EmergencyHandler;
+//import org.ebookdroid.core.log.EmergencyHandler;
 import org.ebookdroid.core.log.LogContext;
 
 import android.app.Application;
@@ -11,7 +11,10 @@ public class EBookDroidApp extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        EmergencyHandler.init(this);
+        //Disable EmergencyHandler. Currently not usefull, couse store file in private space
+        //Also FC in some devices.
+        //Need additional work.
+        //EmergencyHandler.init(this);
         LogContext.init(this);
     }
 }

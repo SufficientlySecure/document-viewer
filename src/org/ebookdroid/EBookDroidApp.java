760a2df12b223b33b0e92fe38e1347db40d1a352
Andrei.Komarovskikh@gmail.com <Andrei.Komarovskikh@gmail.com@714301b5-5bf2-3aa6-4ccc-ecebab0412e3>
2011-08-23 09:12:36 +0000

    Permission changed
    git-svn-id: http://ebookdroid.googlecode.com/svn/trunk@193 714301b5-5bf2-3aa6-4ccc-ecebab0412e3

--- src/org/ebookdroid/EBookDroidApp.java
+++ src/org/ebookdroid/EBookDroidApp.java
@@ -1,6 +1,6 @@
 package org.ebookdroid;
 
-//import org.ebookdroid.core.log.EmergencyHandler;
+import org.ebookdroid.core.log.EmergencyHandler;
 import org.ebookdroid.core.log.LogContext;
 import org.ebookdroid.core.settings.SettingsManager;
 
@@ -15,7 +15,7 @@ public class EBookDroidApp extends Application {
         // Disable EmergencyHandler. Currently not usefull, couse store file in private space
         // Also FC in some devices.
         // Need additional work.
-        // EmergencyHandler.init(this);
+        EmergencyHandler.init(this);
         LogContext.init(this);
         SettingsManager.init(this);
     }

ceed60c5b602df9eb8111dad3bade403531fc552
Andrei.Komarovskikh@gmail.com <Andrei.Komarovskikh@gmail.com@714301b5-5bf2-3aa6-4ccc-ecebab0412e3>
2011-08-25 12:11:37 +0000

    zoom of rects added
    git-svn-id: http://ebookdroid.googlecode.com/svn/trunk@209 714301b5-5bf2-3aa6-4ccc-ecebab0412e3

--- src/org/ebookdroid/utils/MathUtils.java
+++ src/org/ebookdroid/utils/MathUtils.java
@@ -12,4 +12,16 @@ public class MathUtils {
     public static Rect rect(final RectF rect) {
         return new Rect((int) rect.left, (int) rect.top, (int) rect.right, (int) rect.bottom);
     }
+    
+    public static RectF zoom(final RectF rect, float zoom) {
+        return new RectF(zoom *  rect.left, zoom * rect.top, zoom * rect.right, zoom * rect.bottom);
+    }
+    
+    public static RectF zoom(final Rect rect, float zoom) {
+        return new RectF(zoom *  rect.left, zoom * rect.top, zoom * rect.right, zoom * rect.bottom);
+    }
+    
+    public static Rect zoom(final float left, final float top, final float right, final float bottom, float zoom) {
+        return new Rect((int)(zoom * left), (int)(zoom * top), (int)(zoom * right), (int)(zoom * bottom));
+    }
 }

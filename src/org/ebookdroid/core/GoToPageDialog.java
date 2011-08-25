8b365daf5764529e2d2360ec52eacc404712e63f
Andrei.Komarovskikh@gmail.com <Andrei.Komarovskikh@gmail.com@714301b5-5bf2-3aa6-4ccc-ecebab0412e3>
2011-08-25 12:52:09 +0000

    Bookmarks WIP
    git-svn-id: http://ebookdroid.googlecode.com/svn/trunk@210 714301b5-5bf2-3aa6-4ccc-ecebab0412e3

--- src/org/ebookdroid/core/GoToPageDialog.java
+++ src/org/ebookdroid/core/GoToPageDialog.java
@@ -6,26 +6,59 @@ import android.app.Dialog;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.SeekBar;
 import android.widget.SeekBar.OnSeekBarChangeListener;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class GoToPageDialog extends Dialog {
 
     private final IViewerActivity base;
+    private ArrayAdapter<Bookmark> adapter;
+
+    private class Bookmark {
+
+        private final int page;
+        private final String name;
+
+        public Bookmark(int page, String name) {
+            super();
+            this.page = page;
+            this.name = name;
+        }
+
+        @Override
+        public String toString() {
+            return name;
+        }
+
+        public int getPage() {
+            return page;
+        }
+    }
 
     public GoToPageDialog(final IViewerActivity base) {
         super(base.getContext());
         this.base = base;
         setTitle(R.string.dialog_title_goto_page);
         setContentView(R.layout.gotopage);
-        
+
         final Button button = (Button) findViewById(R.id.goToButton);
         final SeekBar seekbar = (SeekBar) findViewById(R.id.seekbar);
         final EditText editText = (EditText) findViewById(R.id.pageNumberTextEdit);
+
+        final Spinner bookmarks = (Spinner) findViewById(R.id.bookmarks);
+
+        adapter = new ArrayAdapter<Bookmark>(base.getContext(), android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        bookmarks.setAdapter(adapter);
 
         button.setOnClickListener(new View.OnClickListener() {
 
@@ -42,7 +75,7 @@ public class GoToPageDialog extends Dialog {
                     goToPageAndDismiss();
                     return true;
                 }
-                
+
                 return false;
             }
         });
@@ -64,6 +97,23 @@ public class GoToPageDialog extends Dialog {
                 }
             }
         });
+
+        bookmarks.setOnItemSelectedListener(new OnItemSelectedListener() {
+
+            @Override
+            public void onItemSelected(AdapterView<?> adapter, View view, int i, long lng) {
+                Object item = adapter.getItemAtPosition(i);
+                if (item instanceof Bookmark) {
+                    Bookmark bookmark = (Bookmark) item;
+                    editText.setText("" + bookmark.getPage());
+                    seekbar.setProgress(bookmark.getPage() - 1);
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapter) {
+            }
+        });
     }
 
     private void goToPageAndDismiss() {
@@ -71,18 +121,23 @@ public class GoToPageDialog extends Dialog {
         dismiss();
     }
 
-    
     @Override
     protected void onStart() {
         super.onStart();
         final SeekBar seekbar = (SeekBar) findViewById(R.id.seekbar);
         final EditText editText = (EditText) findViewById(R.id.pageNumberTextEdit);
-        
+
         seekbar.setMax(base.getDocumentModel().getPageCount() - 1);
         seekbar.setProgress(base.getDocumentModel().getCurrentViewPageIndex());
         editText.setText("" + (base.getDocumentModel().getCurrentViewPageIndex() + 1));
-    }
 
+        if (adapter != null) {
+            adapter.clear();
+            for (int i = 1; i <= base.getDocumentModel().getPageCount(); i += 37) {
+                adapter.add(new Bookmark(i, "Page " + i));
+            }
+        }
+    }
 
     private void navigateToPage() {
         final EditText text = (EditText) findViewById(R.id.pageNumberTextEdit);

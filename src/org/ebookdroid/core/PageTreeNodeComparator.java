9f127826a6ab19767e61a9c3323e3ab29baede2a
alexander.v.kasatkin <alexander.v.kasatkin@714301b5-5bf2-3aa6-4ccc-ecebab0412e3>
2011-09-02 11:40:06 +0000

    Fix decoding order in continious mode.
    git-svn-id: http://ebookdroid.googlecode.com/svn/trunk@288 714301b5-5bf2-3aa6-4ccc-ecebab0412e3

--- src/org/ebookdroid/core/PageTreeNodeComparator.java
+++ src/org/ebookdroid/core/PageTreeNodeComparator.java
@@ -23,33 +23,31 @@ public class PageTreeNodeComparator implements Comparator<PageTreeNode> {
         final boolean v1 = viewState.isNodeVisible(node1, viewState.getBounds(node1.page));
         final boolean v2 = viewState.isNodeVisible(node2, viewState.getBounds(node2.page));
 
+        final RectF s1 = node1.pageSliceBounds;
+        final RectF s2 = node2.pageSliceBounds;
+
         int res = 0;
 
-        if (v1 && !v2) {
+        if (viewIndex1 == cp && viewIndex2 == cp) {
+            res = CompareUtils.compare(s1.top, s2.top);
+            if (res == 0) {
+                res = CompareUtils.compare(s1.left, s2.left);
+            }
+        } else if (v1 && !v2) {
             res = -1;
         } else if (!v1 && v2) {
             res = 1;
         } else {
-
-            final RectF s1 = node1.pageSliceBounds;
-            final RectF s2 = node2.pageSliceBounds;
-
-            if (viewIndex1 == cp && viewIndex2 == cp) {
-                res = CompareUtils.compare(s1.top, s2.top);
-                if (res == 0) {
-                    res = CompareUtils.compare(s1.left, s2.left);
-                }
-            } else {
-                final float d1 = viewIndex1 + s1.top - (cp + 0.5f);
-                final float d2 = viewIndex2 + s2.top - (cp + 0.5f);
-                final int dist1 = Math.abs((int) (d1 * node1.childrenZoomThreshold));
-                final int dist2 = Math.abs((int) (d2 * node2.childrenZoomThreshold));
-                res = CompareUtils.compare(dist1, dist2);
-                if (res == 0) {
-                    res = -CompareUtils.compare(viewIndex1, viewIndex2);
-                }
+            final float d1 = viewIndex1 + s1.top - (cp + 0.5f);
+            final float d2 = viewIndex2 + s2.top - (cp + 0.5f);
+            final int dist1 = Math.abs((int) (d1 * node1.childrenZoomThreshold));
+            final int dist2 = Math.abs((int) (d2 * node2.childrenZoomThreshold));
+            res = CompareUtils.compare(dist1, dist2);
+            if (res == 0) {
+                res = -CompareUtils.compare(viewIndex1, viewIndex2);
             }
         }
+
         return res;
     }
 }

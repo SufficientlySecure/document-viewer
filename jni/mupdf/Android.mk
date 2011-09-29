300273bdc33421d4da30f3d031a8c92f78b7aaa9
mc.creat <mc.creat@714301b5-5bf2-3aa6-4ccc-ecebab0412e3>
2011-09-29 09:01:53 +0000

    Zip64 support in XPS
    git-svn-id: http://ebookdroid.googlecode.com/svn/trunk@425 714301b5-5bf2-3aa6-4ccc-ecebab0412e3

--- jni/mupdf/Android.mk
+++ jni/mupdf/Android.mk
@@ -34,7 +34,6 @@ MY_FREETYPE_SRC_FILES := \
 	freetype/src/base/ftbase.c \
 	freetype/src/base/ftbbox.c \
 	freetype/src/base/ftglyph.c \
-	freetype/src/base/ftbdf.c \
 	freetype/src/base/ftbitmap.c \
 	freetype/src/base/ftcid.c \
 	freetype/src/base/ftfstype.c \
@@ -44,33 +43,36 @@ MY_FREETYPE_SRC_FILES := \
 	freetype/src/base/ftmm.c \
 	freetype/src/base/ftotval.c \
 	freetype/src/base/ftpatent.c \
-	freetype/src/base/ftpfr.c \
 	freetype/src/base/ftstroke.c \
 	freetype/src/base/ftsynth.c \
 	freetype/src/base/fttype1.c \
-	freetype/src/base/ftwinfnt.c \
 	freetype/src/base/ftxf86.c \
-	freetype/src/bdf/bdf.c \
 	freetype/src/cff/cff.c \
 	freetype/src/cid/type1cid.c \
-	freetype/src/pcf/pcf.c \
-	freetype/src/pfr/pfr.c \
 	freetype/src/sfnt/sfnt.c \
 	freetype/src/truetype/truetype.c \
 	freetype/src/type1/type1.c \
-	freetype/src/type42/type42.c \
-	freetype/src/winfonts/winfnt.c \
 	freetype/src/raster/raster.c \
 	freetype/src/smooth/smooth.c \
-	freetype/src/autofit/autofit.c \
 	freetype/src/cache/ftcache.c \
 	freetype/src/gzip/ftgzip.c \
-	freetype/src/lzw/ftlzw.c \
 	freetype/src/gxvalid/gxvalid.c \
 	freetype/src/otvalid/otvalid.c \
 	freetype/src/psaux/psaux.c \
 	freetype/src/pshinter/pshinter.c \
 	freetype/src/psnames/psnames.c
+
+#	freetype/src/base/ftpfr.c \
+#	freetype/src/base/ftbdf.c \
+#	freetype/src/base/ftwinfnt.c \
+
+#	freetype/src/autofit/autofit.c \
+#	freetype/src/pfr/pfr.c \
+#	freetype/src/type42/type42.c \
+#	freetype/src/pcf/pcf.c \
+#	freetype/src/bdf/bdf.c \
+#	freetype/src/winfonts/winfnt.c \
+#	freetype/src/lzw/ftlzw.c \
 
 # jbig2dec
 MY_JBIG2DEC_CFLAGS := -DHAVE_CONFIG_H

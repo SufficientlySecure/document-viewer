3cff9d1083eeab028f28421311986d6a4027c733
alexander.v.kasatkin <alexander.v.kasatkin@714301b5-5bf2-3aa6-4ccc-ecebab0412e3>
2012-01-15 00:47:18 +0000

    FB2 use native memory to store text.
    git-svn-id: http://ebookdroid.googlecode.com/svn/trunk@751 714301b5-5bf2-3aa6-4ccc-ecebab0412e3

--- src/org/ebookdroid/fb2droid/codec/FB2TextElement.java
+++ src/org/ebookdroid/fb2droid/codec/FB2TextElement.java
@@ -4,23 +4,18 @@ import android.graphics.Canvas;
 
 public class FB2TextElement extends AbstractFB2LineElement {
 
-    public final char[] chars;
-    public final int start;
-    public final int length;
-
+    public final CharSequence chars;
     public final RenderingStyle renderingState;
 
-    public FB2TextElement(final char[] ch, final int st, final int len, final RenderingStyle renderingState) {
-        super(renderingState.paint.measureText(ch, st, len), renderingState.textSize);
+    public FB2TextElement(final CharSequence ch, final float width, final RenderingStyle renderingState) {
+        super(width, renderingState.textSize);
         this.chars = ch;
-        this.start = st;
-        this.length = len;
         this.renderingState = renderingState;
     }
 
     @Override
     public float render(final Canvas c, final int y, final int x, final float additionalWidth) {
-        c.drawText(chars, start, length, x, renderingState.superScript ? y - renderingState.textSize
+        c.drawText(chars, 0, chars.length(), x, renderingState.superScript ? y - renderingState.textSize
                 : renderingState.subScript ? y + renderingState.textSize / 2 : y, renderingState.paint);
         return width;
     }

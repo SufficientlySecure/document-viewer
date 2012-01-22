efa13d6e5307d5aa873438682a790a40e6404659
alexander.v.kasatkin <alexander.v.kasatkin@714301b5-5bf2-3aa6-4ccc-ecebab0412e3>
2012-01-22 10:58:14 +0000

    Release archives on finalize()
    git-svn-id: http://ebookdroid.googlecode.com/svn/trunk@814 714301b5-5bf2-3aa6-4ccc-ecebab0412e3

--- src/org/ebookdroid/core/utils/archives/ArchiveFile.java
+++ src/org/ebookdroid/core/utils/archives/ArchiveFile.java
@@ -1,16 +1,15 @@
 package org.ebookdroid.core.utils.archives;
 
+import java.io.Closeable;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Enumeration;
 
-public interface ArchiveFile<ArchiveEntryType extends ArchiveEntry> {
+public interface ArchiveFile<ArchiveEntryType extends ArchiveEntry> extends Closeable {
 
     boolean randomAccessAllowed();
 
     Enumeration<ArchiveEntryType> entries();
 
     InputStream open(ArchiveEntryType entry) throws IOException;
-
-    void close() throws IOException;
 }

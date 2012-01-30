aafb97f438d54f367728f554eb8785fd4e82bc53
Andrei.Komarovskikh@gmail.com <Andrei.Komarovskikh@gmail.com@714301b5-5bf2-3aa6-4ccc-ecebab0412e3>
2012-01-30 12:09:27 +0000

    Fix for 138
    git-svn-id: http://ebookdroid.googlecode.com/svn/trunk@871 714301b5-5bf2-3aa6-4ccc-ecebab0412e3

--- src/org/ebookdroid/core/touch/MultiTouchZoomImpl.java
+++ src/org/ebookdroid/core/touch/MultiTouchZoomImpl.java
@@ -47,10 +47,10 @@ public class MultiTouchZoomImpl implements IMultiTouchZoom {
     private float getZoomDistance(final MotionEvent ev) {
         if (ev.getPointerCount() > 1) {
             // We do not need actual distance. Square also goes well.
-            float x0 = ev.getX(ev.getPointerId(0));
-            float x1 = ev.getX(ev.getPointerId(1));
-            float y0 = ev.getY(ev.getPointerId(0));
-            float y1 = ev.getY(ev.getPointerId(1));
+            float x0 = ev.getX(0);
+            float x1 = ev.getX(1);
+            float y0 = ev.getY(0);
+            float y1 = ev.getY(1);
             return (float) ((x0 - x1) * (x0 - x1) + (y0 - y1) * (y0 - y1));
         }
         return lastZoomDistance;

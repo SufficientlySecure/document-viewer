d69e932b67d8832e78ab031fd198e0512b764c19
alexander.v.kasatkin <alexander.v.kasatkin@714301b5-5bf2-3aa6-4ccc-ecebab0412e3>
2012-05-31 09:43:59 +0000

    Full rotation modes implementation (#236).
    git-svn-id: http://ebookdroid.googlecode.com/svn/trunk@1318 714301b5-5bf2-3aa6-4ccc-ecebab0412e3

--- src/org/ebookdroid/common/settings/types/RotationType.java
+++ src/org/ebookdroid/common/settings/types/RotationType.java
@@ -5,29 +5,70 @@ import org.ebookdroid.R;
 
 import android.content.pm.ActivityInfo;
 
+import org.emdev.utils.android.AndroidVersion;
 import org.emdev.utils.enums.ResourceConstant;
 
 public enum RotationType implements ResourceConstant {
+
     /**
-     *
-     */
-    AUTOMATIC(R.string.pref_rotation_auto, ActivityInfo.SCREEN_ORIENTATION_SENSOR),
+    *
+    */
+    UNSPECIFIED(R.string.pref_rotation_unspecified, ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED, 3),
     /**
-     *
-     */
-    LANDSCAPE(R.string.pref_rotation_land, ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE),
+    *
+    */
+    LANDSCAPE(R.string.pref_rotation_land, ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE, 3),
     /**
-     *
-     */
-    PORTRAIT(R.string.pref_rotation_port, ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+    *
+    */
+    PORTRAIT(R.string.pref_rotation_port, ActivityInfo.SCREEN_ORIENTATION_PORTRAIT, 3),
+    /**
+   *
+   */
+    USER(R.string.pref_rotation_user, ActivityInfo.SCREEN_ORIENTATION_USER, 3),
+    /**
+    *
+    */
+    BEHIND(R.string.pref_rotation_behind, ActivityInfo.SCREEN_ORIENTATION_BEHIND, 3),
+    /**
+    *
+    */
+    AUTOMATIC(R.string.pref_rotation_auto, ActivityInfo.SCREEN_ORIENTATION_SENSOR, 3),
+    /**
+    *
+    */
+    NOSENSOR(R.string.pref_rotation_nosensor, ActivityInfo.SCREEN_ORIENTATION_NOSENSOR, 3),
+    /**
+    *
+    */
+    SENSOR_LANDSCAPE(R.string.pref_rotation_sensor_landscape, ActivityInfo.SCREEN_ORIENTATION_SENSOR_LANDSCAPE, 10),
+    /**
+    *
+    */
+    SENSOR_PORTRAIT(R.string.pref_rotation_sensor_portrait, ActivityInfo.SCREEN_ORIENTATION_SENSOR_PORTRAIT, 10),
+    /**
+    *
+    */
+    REVERSE_LANDSCAPE(R.string.pref_rotation_reverse_landscape, ActivityInfo.SCREEN_ORIENTATION_REVERSE_LANDSCAPE, 10),
+    /**
+    *
+    */
+    REVERSE_PORTRAIT(R.string.pref_rotation_reverse_portrait, ActivityInfo.SCREEN_ORIENTATION_REVERSE_PORTRAIT, 10),
+    /**
+    *
+    */
+    FULL_SENSOR(R.string.pref_rotation_full_sensor, ActivityInfo.SCREEN_ORIENTATION_FULL_SENSOR, 10);
 
     private final String resValue;
 
     private final int orientation;
 
-    private RotationType(final int resId, final int orientation) {
+    private final int version;
+
+    private RotationType(final int resId, final int orientation, final int version) {
         this.resValue = EBookDroidApp.context.getString(resId);
         this.orientation = orientation;
+        this.version = version;
     }
 
     public String getResValue() {
@@ -35,6 +76,10 @@ public enum RotationType implements ResourceConstant {
     }
 
     public int getOrientation() {
-        return orientation;
+        return this.version <= AndroidVersion.VERSION ? orientation : ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED;
+    }
+
+    public int getVersion() {
+        return version;
     }
 }

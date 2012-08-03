dcfb2bf5ad97a9fcbd077c10f1a37519f0b4e235
alexander.v.kasatkin <alexander.v.kasatkin@714301b5-5bf2-3aa6-4ccc-ecebab0412e3>
2012-08-03 15:30:47 +0000

    Fix for x86 compatibility.
    git-svn-id: http://ebookdroid.googlecode.com/svn/trunk@1494 714301b5-5bf2-3aa6-4ccc-ecebab0412e3

--- jni/mupdf/mupdf/fitz/fitz.h
+++ jni/mupdf/mupdf/fitz/fitz.h
@@ -29,6 +29,7 @@
 #endif
 
 #ifdef __ANDROID__
+#undef HAVE_SIGSETJMP
 #include <android/log.h>
 #define LOG_TAG "libmupdf"
 #define LOGI(...) __android_log_print(ANDROID_LOG_INFO,LOG_TAG,__VA_ARGS__)

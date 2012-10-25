662aaf08a333f18c222763c958eb1114c614901c
alexander.v.kasatkin <alexander.v.kasatkin@714301b5-5bf2-3aa6-4ccc-ecebab0412e3>
2012-10-25 14:44:18 +0000

    [FB2] Fix text measuring
    git-svn-id: http://ebookdroid.googlecode.com/svn/trunk@1806 714301b5-5bf2-3aa6-4ccc-ecebab0412e3

--- src/org/emdev/common/textmarkup/CustomTextPaint.java
+++ src/org/emdev/common/textmarkup/CustomTextPaint.java
@@ -45,18 +45,46 @@ public class CustomTextPaint extends TextPaint {
         initMeasures(0x0000, standard);
         initMeasures(0x0100, latin1);
         initMeasures(0x0400, rus);
-        initMeasures(0x2000, punct);
+        initMeasures(0x2000, punct, 0x00, 0x0B);
+        initMeasures(0x2000, punct, 0x10, 0x27);
+        initMeasures(0x2000, punct, 0x30, 0x3F);
+        initMeasures(0x2000, punct, 0x40, 0x4F);
+        initMeasures(0x2000, punct, 0x50, 0x5F);
+        initMeasures(0x2000, punct, 0xD0, 0xDF);
+        initMeasures(0x2000, punct, 0xE0, 0xEF);
+        initMeasures(0x2000, punct, 0xF0, 0xF0);
     }
 
-    private void initMeasures(int codepage, float[] widths) {
+    private void initMeasures(final int codepage, final float[] widths) {
         final char[] chars = new char[256];
         for (int i = 0; i < 256; i++) {
             chars[i] = (char) (i + codepage);
         }
         this.getTextWidths(chars, 0, chars.length, widths);
+        for (int i = 0; i < chars.length; i++) {
+            if (widths[i] == 0) {
+                widths[i] = super.measureText(chars, i, 1);
+            }
+        }
     }
 
-    private char[] temp = new char[256];
+    private void initMeasures(final int codepage, final float[] widths, final int start, final int end) {
+        final int length = end - start + 1;
+        final char[] chars = new char[length];
+        for (int i = 0; i < length; i++) {
+            chars[i] = (char) (i + start + codepage);
+        }
+        final float[] w = new float[length];
+        this.getTextWidths(chars, 0, length, widths);
+        for (int i = 0; i < length; i++) {
+            if (w[i] == 0) {
+                w[i] = super.measureText(chars, i, 1);
+            }
+            widths[chars[i] - codepage] = w[i];
+        }
+    }
+
+    private final char[] temp = new char[256];
 
     @Override
     public float measureText(final char[] text, final int index, final int count) {

ab7591782359ef04474c18c93a747ba37464db14
alexander.v.kasatkin <alexander.v.kasatkin@714301b5-5bf2-3aa6-4ccc-ecebab0412e3>
2012-11-02 12:46:32 +0000

    Show multi-pane preferences only on tablets and only in landscape.
    git-svn-id: http://ebookdroid.googlecode.com/svn/trunk@1843 714301b5-5bf2-3aa6-4ccc-ecebab0412e3

--- src/org/ebookdroid/ui/settings/FragmentedSettingsActivity.java
+++ src/org/ebookdroid/ui/settings/FragmentedSettingsActivity.java
@@ -3,12 +3,12 @@ package org.ebookdroid.ui.settings;
 import org.ebookdroid.R;
 
 import android.annotation.TargetApi;
+import android.content.res.Configuration;
 
 import java.util.List;
 
 @TargetApi(11)
 public class FragmentedSettingsActivity extends SettingsActivity {
-
 
     @Override
     protected void onCreate() {
@@ -17,5 +17,14 @@ public class FragmentedSettingsActivity extends SettingsActivity {
     @Override
     public void onBuildHeaders(final List<Header> target) {
         loadHeadersFromResource(R.xml.preferences_headers, target);
+    }
+
+    @Override
+    public boolean onIsMultiPane() {
+        final Configuration c = this.getResources().getConfiguration();
+        if (0 != (Configuration.SCREENLAYOUT_SIZE_XLARGE & c.screenLayout)) {
+            return c.orientation == Configuration.ORIENTATION_LANDSCAPE;
+        }
+        return false;
     }
 }

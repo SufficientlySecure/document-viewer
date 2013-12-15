62a415306e4be2fe514b7a1b4d2b1c619f2bc059
Dominik Schürmann <dominik@dominikschuermann.de>
2013-12-15 04:59:24 +0100

    Fix crashes due to API 19, Version 2.4

--- document-viewer/src/org/ebookdroid/ui/settings/FragmentedSettingsActivity.java
+++ document-viewer/src/org/ebookdroid/ui/settings/FragmentedSettingsActivity.java
@@ -1,5 +1,16 @@
+
 package org.ebookdroid.ui.settings;
 
+import org.ebookdroid.ui.settings.fragments.BackupFragment;
+import org.ebookdroid.ui.settings.fragments.BasePreferenceFragment;
+import org.ebookdroid.ui.settings.fragments.BrowserFragment;
+import org.ebookdroid.ui.settings.fragments.NavFragment;
+import org.ebookdroid.ui.settings.fragments.OpdsFragment;
+import org.ebookdroid.ui.settings.fragments.PerformanceFragment;
+import org.ebookdroid.ui.settings.fragments.RenderFragment;
+import org.ebookdroid.ui.settings.fragments.ScrollFragment;
+import org.ebookdroid.ui.settings.fragments.TypeSpecificFragment;
+import org.ebookdroid.ui.settings.fragments.UIFragment;
 import org.sufficientlysecure.viewer.R;
 
 import android.annotation.TargetApi;
@@ -24,6 +35,22 @@ public class FragmentedSettingsActivity extends SettingsActivity {
         final Configuration c = this.getResources().getConfiguration();
         if (0 != (Configuration.SCREENLAYOUT_SIZE_XLARGE & c.screenLayout)) {
             return c.orientation == Configuration.ORIENTATION_LANDSCAPE;
+        }
+        return false;
+    }
+
+    protected boolean isValidFragment(String fragmentName) {
+        if (ScrollFragment.class.getName().equals(fragmentName)
+                || UIFragment.class.getName().equals(fragmentName)
+                || NavFragment.class.getName().equals(fragmentName)
+                || PerformanceFragment.class.getName().equals(fragmentName)
+                || TypeSpecificFragment.class.getName().equals(fragmentName)
+                || BackupFragment.class.getName().equals(fragmentName)
+                || BasePreferenceFragment.class.getName().equals(fragmentName)
+                || BrowserFragment.class.getName().equals(fragmentName)
+                || OpdsFragment.class.getName().equals(fragmentName)
+                || RenderFragment.class.getName().equals(fragmentName)) {
+            return true;
         }
         return false;
     }

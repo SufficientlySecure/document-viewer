0f26e5e466cd32a1db8538248b7cf1a59babb997
Eric Wasylishen <ewasylishen@gmail.com>
2016-02-10 12:01:35 -0700

    mupdf: Add patch for fixing -NOCJK in 1.8. Patch is from MuPDF commit 3116d629d72ff7bf227a46601d97db8d283c0650

--- init.sh
+++ init.sh
@@ -6,6 +6,7 @@ cd document-viewer/jni/mupdf/mupdf
 make generate
 patch -p1 < ../overrides/fonts.patch
 patch -p1 < ../overrides/nightmode_slowcmyk.patch
+patch -p1 < ../overrides/nocjk.patch
 
 cd ../../djvu/djvulibre
 patch -p1 < ../overrides/remove_print_save_api.patch

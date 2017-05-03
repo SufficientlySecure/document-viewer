4e9c3c35d0ac2e5c38759454f3e305aabb28a35a
SASAKI Suguru <suguru@sonik.org>
2017-05-04 07:36:03 +0900

    Update mupdf to v1.11

--- document-viewer/jni/mupdf/ThirdParty.mk
+++ document-viewer/jni/mupdf/ThirdParty.mk
@@ -9,12 +9,12 @@ LOCAL_C_INCLUDES := \
 	$(MY_ROOT)/thirdparty/harfbuzz/src \
 	$(MY_ROOT)/thirdparty/jbig2dec \
 	$(MY_ROOT)/thirdparty/openjpeg/src/lib/openjp2 \
-	$(MY_ROOT)/thirdparty/jpeg \
+	$(MY_ROOT)/thirdparty/libjpeg \
 	$(MY_ROOT)/thirdparty/mujs \
 	$(MY_ROOT)/thirdparty/zlib \
 	$(MY_ROOT)/thirdparty/freetype/include \
 	$(MY_ROOT)/scripts/freetype \
-	$(MY_ROOT)/scripts/jpeg
+	$(MY_ROOT)/scripts/libjpeg
 
 LOCAL_CFLAGS := \
 	-DFT2_BUILD_LIBRARY -DDARWIN_NO_CARBON -DHAVE_STDINT_H \
@@ -111,36 +111,36 @@ LOCAL_SRC_FILES := \
 	$(MY_ROOT)/thirdparty/openjpeg/src/lib/openjp2/thix_manager.c \
 	$(MY_ROOT)/thirdparty/openjpeg/src/lib/openjp2/thread.c \
 	$(MY_ROOT)/thirdparty/openjpeg/src/lib/openjp2/tpix_manager.c \
-	$(MY_ROOT)/thirdparty/jpeg/jaricom.c \
-	$(MY_ROOT)/thirdparty/jpeg/jcomapi.c \
-	$(MY_ROOT)/thirdparty/jpeg/jdapimin.c \
-	$(MY_ROOT)/thirdparty/jpeg/jdapistd.c \
-	$(MY_ROOT)/thirdparty/jpeg/jdarith.c \
-	$(MY_ROOT)/thirdparty/jpeg/jdatadst.c \
-	$(MY_ROOT)/thirdparty/jpeg/jdatasrc.c \
-	$(MY_ROOT)/thirdparty/jpeg/jdcoefct.c \
-	$(MY_ROOT)/thirdparty/jpeg/jdcolor.c \
-	$(MY_ROOT)/thirdparty/jpeg/jddctmgr.c \
-	$(MY_ROOT)/thirdparty/jpeg/jdhuff.c \
-	$(MY_ROOT)/thirdparty/jpeg/jdinput.c \
-	$(MY_ROOT)/thirdparty/jpeg/jdmainct.c \
-	$(MY_ROOT)/thirdparty/jpeg/jdmarker.c \
-	$(MY_ROOT)/thirdparty/jpeg/jdmaster.c \
-	$(MY_ROOT)/thirdparty/jpeg/jdmerge.c \
-	$(MY_ROOT)/thirdparty/jpeg/jdpostct.c \
-	$(MY_ROOT)/thirdparty/jpeg/jdsample.c \
-	$(MY_ROOT)/thirdparty/jpeg/jdtrans.c \
-	$(MY_ROOT)/thirdparty/jpeg/jerror.c \
-	$(MY_ROOT)/thirdparty/jpeg/jfdctflt.c \
-	$(MY_ROOT)/thirdparty/jpeg/jfdctfst.c \
-	$(MY_ROOT)/thirdparty/jpeg/jfdctint.c \
-	$(MY_ROOT)/thirdparty/jpeg/jidctflt.c \
-	$(MY_ROOT)/thirdparty/jpeg/jidctfst.c \
-	$(MY_ROOT)/thirdparty/jpeg/jidctint.c \
-	$(MY_ROOT)/thirdparty/jpeg/jmemmgr.c \
-	$(MY_ROOT)/thirdparty/jpeg/jquant1.c \
-	$(MY_ROOT)/thirdparty/jpeg/jquant2.c \
-	$(MY_ROOT)/thirdparty/jpeg/jutils.c \
+	$(MY_ROOT)/thirdparty/libjpeg/jaricom.c \
+	$(MY_ROOT)/thirdparty/libjpeg/jcomapi.c \
+	$(MY_ROOT)/thirdparty/libjpeg/jdapimin.c \
+	$(MY_ROOT)/thirdparty/libjpeg/jdapistd.c \
+	$(MY_ROOT)/thirdparty/libjpeg/jdarith.c \
+	$(MY_ROOT)/thirdparty/libjpeg/jdatadst.c \
+	$(MY_ROOT)/thirdparty/libjpeg/jdatasrc.c \
+	$(MY_ROOT)/thirdparty/libjpeg/jdcoefct.c \
+	$(MY_ROOT)/thirdparty/libjpeg/jdcolor.c \
+	$(MY_ROOT)/thirdparty/libjpeg/jddctmgr.c \
+	$(MY_ROOT)/thirdparty/libjpeg/jdhuff.c \
+	$(MY_ROOT)/thirdparty/libjpeg/jdinput.c \
+	$(MY_ROOT)/thirdparty/libjpeg/jdmainct.c \
+	$(MY_ROOT)/thirdparty/libjpeg/jdmarker.c \
+	$(MY_ROOT)/thirdparty/libjpeg/jdmaster.c \
+	$(MY_ROOT)/thirdparty/libjpeg/jdmerge.c \
+	$(MY_ROOT)/thirdparty/libjpeg/jdpostct.c \
+	$(MY_ROOT)/thirdparty/libjpeg/jdsample.c \
+	$(MY_ROOT)/thirdparty/libjpeg/jdtrans.c \
+	$(MY_ROOT)/thirdparty/libjpeg/jerror.c \
+	$(MY_ROOT)/thirdparty/libjpeg/jfdctflt.c \
+	$(MY_ROOT)/thirdparty/libjpeg/jfdctfst.c \
+	$(MY_ROOT)/thirdparty/libjpeg/jfdctint.c \
+	$(MY_ROOT)/thirdparty/libjpeg/jidctflt.c \
+	$(MY_ROOT)/thirdparty/libjpeg/jidctfst.c \
+	$(MY_ROOT)/thirdparty/libjpeg/jidctint.c \
+	$(MY_ROOT)/thirdparty/libjpeg/jmemmgr.c \
+	$(MY_ROOT)/thirdparty/libjpeg/jquant1.c \
+	$(MY_ROOT)/thirdparty/libjpeg/jquant2.c \
+	$(MY_ROOT)/thirdparty/libjpeg/jutils.c \
 	$(MY_ROOT)/thirdparty/zlib/adler32.c \
 	$(MY_ROOT)/thirdparty/zlib/compress.c \
 	$(MY_ROOT)/thirdparty/zlib/crc32.c \
@@ -151,6 +151,10 @@ LOCAL_SRC_FILES := \
 	$(MY_ROOT)/thirdparty/zlib/trees.c \
 	$(MY_ROOT)/thirdparty/zlib/uncompr.c \
 	$(MY_ROOT)/thirdparty/zlib/zutil.c \
+	$(MY_ROOT)/thirdparty/zlib/gzlib.c \
+	$(MY_ROOT)/thirdparty/zlib/gzwrite.c \
+	$(MY_ROOT)/thirdparty/zlib/gzclose.c \
+	$(MY_ROOT)/thirdparty/zlib/gzread.c \
 	$(MY_ROOT)/thirdparty/freetype/src/base/ftbase.c \
 	$(MY_ROOT)/thirdparty/freetype/src/base/ftbbox.c \
 	$(MY_ROOT)/thirdparty/freetype/src/base/ftbitmap.c \

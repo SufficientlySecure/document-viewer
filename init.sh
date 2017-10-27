0bfb13b1b65cc27145b4526a3c2564ecdb468674
Eric Wasylishen <ewasylishen@gmail.com>
2017-10-27 12:09:07 -0600

    Apply patches for CVE-2017-14685 CVE-2017-14686 CVE-2017-14687 CVE-2017-15587 from mupdf git see: https://gitlab.com/fdroid/fdroiddata/issues/956

--- init.sh
+++ init.sh
@@ -7,6 +7,10 @@ cd document-viewer/jni/mupdf/mupdf
 make generate
 #patch -p1 < ../overrides/fonts.patch
 patch -p1 < ../overrides/nightmode_slowcmyk.patch
+patch -p1 < ../overrides/cve-2017-14685.patch
+patch -p1 < ../overrides/cve-2017-14686.patch
+patch -p1 < ../overrides/cve-2017-14687.patch
+patch -p1 < ../overrides/cve-2017-15587.patch
 
 cd ../../djvu/djvulibre
 patch -p1 < ../overrides/remove_print_save_api.patch

2b3284ae4d7a4c5f3ccb86d8418c4a06959965d6
Eric Wasylishen <ewasylishen@gmail.com>
2019-03-15 02:08:12 -0600

    Target api 28, add new main activity that requests WRITE_EXTERNAL_STORAGE

---
+++ document-viewer/src/main/java/org/ebookdroid/ui/library/RequestPermissionsActivity.java
@@ -0,0 +1,67 @@
+package org.ebookdroid.ui.library;
+
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import org.sufficientlysecure.viewer.R;
+
+public class RequestPermissionsActivity extends AppCompatActivity {
+
+    private static final int DV_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE = 10;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    DV_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE);
+        } else {
+            onSuccess();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String[] permissions,
+                                           int[] grantResults) {
+        switch (requestCode) {
+            case DV_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    onSuccess();
+                } else {
+                    onFailure();
+                }
+                break;
+            }
+        }
+    }
+
+    private void onSuccess() {
+        Intent intent = new Intent(this, RecentActivity.class);
+        startActivity(intent);
+
+        finish();
+    }
+
+    private void onFailure() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(R.string.error_write_external_storage_permission);
+        builder.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                finish();
+            }
+        });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+}
